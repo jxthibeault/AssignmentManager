f2472fccfdafb12b137cd3c8b65a9c03a4f9da03
Josh Thibeault <jxthibeault@gmail.com>
2018-12-07 12:00:53 -0500

    Wrote skeleton and method stubs for TimeSpan

--- src/AssignmentManager/TimeSpan.java
+++ src/AssignmentManager/TimeSpan.java
@@ -1,14 +1,82 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package AssignmentManager;
 
 /**
+ * Description: A class representing a span of time from a start hour to an end
+ * hour.
  *
  * @author Josh Thibeault
  */
-public class TimeSpan {
+public class TimeSpan implements Comparable<TimeSpan> {
     
+    private int startHour;
+    private int endHour;
+    
+    /**
+     * 
+     */
+    public TimeSpan() {
+        
+    }
+    
+    /**
+     * 
+     * @param startHour
+     * @param endHour 
+     */
+    public TimeSpan(int startHour, int endHour) {
+        
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    public int getSpanLength() {
+        
+        return 0;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    public int getStartHour() {
+        return this.startHour;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    public int getEndHour() {
+        return this.endHour;
+    }
+    
+    /**
+     * 
+     * @param that
+     * @return 
+     */
+    @Override
+    public int compareTo(TimeSpan that) {
+        return 0;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    @Override
+    public String toString() {
+        return null;
+    }
+    
+    /**
+     * 
+     * @param args 
+     */
+    public static void main(String[] args) {
+        
+    }
+
 }
