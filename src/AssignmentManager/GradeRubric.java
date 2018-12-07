c72dce86dd477e010bbdf9a25ba15257cc684a21
Josh Thibeault <jxthibeault@gmail.com>
2018-12-07 13:16:10 -0500

    Made several fields final in GradeRubric for performance optimization. Removed unused imports from GradeRubric. Wrote toString() in GradeRubric with unit tests (pass).

--- src/AssignmentManager/GradeRubric.java
+++ src/AssignmentManager/GradeRubric.java
@@ -1,7 +1,6 @@
 package AssignmentManager;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Map.Entry;
 import java.util.Set;
 import java.util.TreeMap;
 
@@ -11,9 +10,9 @@ import java.util.TreeMap;
  *
  * @author Josh Thibeault
  */
-public class GradeRubric {
+public final class GradeRubric {
 
-    private TreeMap<String, Double> categories;
+    private final TreeMap<String, Double> categories;
 
     /**
      * Description: Default constructor for GradeRubric. Initializes an empty
@@ -53,7 +52,7 @@ public class GradeRubric {
     /**
      * Description: Gets and returns an array of the names of all the categories
      * contained within this GradeRubric.
-     * 
+     *
      * @return an array of the names of all categories in this rubric
      */
     public Set<String> getCategories() {
@@ -72,24 +71,52 @@ public class GradeRubric {
     }
 
     /**
+     * Description: Generates and returns a String representation of this
+     * GradeRubric object. The returned String is a table containing each
+     * category and its associated weight. The category column is 20 characters
+     * wide, and the weight column is 6 characters wide.
      *
-     * @return
+     * @return a string representation of the GradeRubric object
      */
     @Override
     public String toString() {
-        return null;
+        StringBuilder builder = new StringBuilder();
+
+        if (this.categories.isEmpty()) {
+            builder.append("No categories in this rubric.");
+        } else {
+            boolean printPercentSign = true;
+            for (Entry<String, Double> entry : this.categories.entrySet()) {
+                builder.append(String.format("%-20s%5.2f",
+                        entry.getKey(), entry.getValue()));
+                if(printPercentSign) {
+                    builder.append("%");
+                    printPercentSign = false;
+                }
+                builder.append(System.lineSeparator());
+            }
+            builder.deleteCharAt(builder.length() - 1);
+        }
+
+        return builder.toString();
     }
 
     /**
+     * Description: Unit tests for the GradeRubric class.
      *
-     * @param args
+     * @param args the command line arguments
      */
     public static void main(String[] args) {
         TreeMap<String, Double> testMap = new TreeMap<>();
-        testMap.put("Homework", 45.00);
-        testMap.put("Tests", 55.00);
-        
+        testMap.put("Exam 1", 20.00);
+        testMap.put("Exam 2", 20.00);
+        testMap.put("Exam 3", 20.00);
+        testMap.put("Homework", 25.00);
+        testMap.put("Quizzes", 15.00);
+
         GradeRubric testRubric = new GradeRubric(testMap);
+        
+        System.out.println(testRubric.toString());
         System.out.println(testRubric.getCategories());
         System.out.println(testRubric.getWeight("Homework"));
         System.out.println(testRubric.getWeight("Attendance"));
