ead6cde464a2557039ed1c921a5c9a1f7cb98747
Josh Thibeault <jxthibeault@gmail.com>
2018-12-07 13:06:10 -0500

    Wrote getter methods for GradeRubric. Updated unit tests for tricky getter methods (pass).

--- src/AssignmentManager/GradeRubric.java
+++ src/AssignmentManager/GradeRubric.java
@@ -1,76 +1,99 @@
 package AssignmentManager;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Set;
 import java.util.TreeMap;
 
 /**
  * Description: A class representing a rubric used to weight assignments based
  * on assignment type for a course when determining a final grade.
- * 
+ *
  * @author Josh Thibeault
  */
 public class GradeRubric {
-    
+
     private TreeMap<String, Double> categories;
-    
+
     /**
      * Description: Default constructor for GradeRubric. Initializes an empty
-     * TreeMap<String, Double> internally, containing no categories or weights.
+     * TreeMap internally, containing no categories or weights.
      */
     public GradeRubric() {
         this.categories = new TreeMap<>();
     }
-    
+
     /**
      * Description: Constructor for GradeRubric accepting a pre-populated
-     * TreeMap<String, Double> containing categories and weights.
-     * 
+     * TreeMap containing categories and weights.
+     *
      * @param categories map with category names as keys and weights as values
      */
     public GradeRubric(TreeMap<String, Double> categories) {
         this.categories = categories;
     }
-    
+
     /**
-     * 
-     * @param key
-     * @return 
+     * Description: Gets and returns the weight associated with a specific
+     * category, or returns a negative value if the category does not exist. The
+     * category lookup is case-sensitive.
+     *
+     * @param category the category to look up the weight for
+     * @return the weight associated with the category, or a negative number if
+     * the category does not exist
      */
-    public double getWeight(String key) {
-        return 0;
+    public double getWeight(String category) {
+        if (this.categories.containsKey(category)) {
+            return this.categories.get(category);
+        } else {
+            return -1;
+        }
     }
-    
+
     /**
+     * Description: Gets and returns an array of the names of all the categories
+     * contained within this GradeRubric.
      * 
-     * @return 
+     * @return an array of the names of all categories in this rubric
      */
-    public ArrayList<String> getCategories() {
-        return null;
+    public Set<String> getCategories() {
+        return this.categories.keySet();
     }
-    
+
     /**
-     * 
-     * @return 
+     * Description: Gets and returns the TreeMap of categories and weights of
+     * which this GradeRubric is composed.
+     *
+     * @return a reference to the TreeMap containing this rubric's categories
+     * and their weights
      */
     public TreeMap<String, Double> getRubric() {
-        return null;
+        return this.categories;
     }
-    
+
     /**
-     * 
-     * @return 
+     *
+     * @return
      */
     @Override
     public String toString() {
         return null;
     }
-    
+
     /**
-     * 
-     * @param args 
+     *
+     * @param args
      */
     public static void main(String[] args) {
+        TreeMap<String, Double> testMap = new TreeMap<>();
+        testMap.put("Homework", 45.00);
+        testMap.put("Tests", 55.00);
         
+        GradeRubric testRubric = new GradeRubric(testMap);
+        System.out.println(testRubric.getCategories());
+        System.out.println(testRubric.getWeight("Homework"));
+        System.out.println(testRubric.getWeight("Attendance"));
+
     }
-    
+
 }
