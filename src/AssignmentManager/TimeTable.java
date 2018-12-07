96067b3ef76165281084c135482fd9bf47fc26db
Josh Thibeault <jxthibeault@gmail.com>
2018-12-07 13:32:50 -0500

    Designed and wrote constructors. Completed JavaDoc for getter methods.

--- src/AssignmentManager/TimeTable.java
+++ src/AssignmentManager/TimeTable.java
@@ -6,97 +6,110 @@ package AssignmentManager;
  *
  * @author Josh Thibeault
  */
-public class TimeTable {
+public final class TimeTable {
 
-    private TimeSpan monday;
-    private TimeSpan tuesday;
-    private TimeSpan wednesday;
-    private TimeSpan thursday;
-    private TimeSpan friday;
+    private final TimeSpan monday;
+    private final TimeSpan tuesday;
+    private final TimeSpan wednesday;
+    private final TimeSpan thursday;
+    private final TimeSpan friday;
 
     /**
-     * 
+     * Description: Default constructor for TimeTable. The default TimeTable
+     * contains no scheduled times for any days of the week.
      */
     public TimeTable() {
-
+        this(null, null, null, null, null);
     }
 
     /**
-     * 
-     * @param monday
-     * @param tuesday
-     * @param wednesday
-     * @param thursday
-     * @param friday 
+     * Description: Constructor for TimeTable accepting a scheduled time for
+     * each day of the week. If a particular day of the week has no scheduled
+     * time, pass in a null reference.
+     *
+     * @param monday time span for Monday
+     * @param tuesday time span for Tuesday
+     * @param wednesday time span for Wednesday
+     * @param thursday time span for Thursday
+     * @param friday time span for Friday
      */
     public TimeTable(TimeSpan monday, TimeSpan tuesday, TimeSpan wednesday,
             TimeSpan thursday, TimeSpan friday) {
-
+        this.monday = monday;
+        this.tuesday = tuesday;
+        this.wednesday = wednesday;
+        this.thursday = thursday;
+        this.friday = friday;
     }
-    
+
     /**
-     * 
-     * @return 
+     * Description: Gets and returns the TimeSpan object for Monday.
+     *
+     * @return a reference to the TimeSpan object for Monday
      */
     public TimeSpan getMondayTimes() {
         return this.monday;
     }
-    
+
     /**
-     * 
-     * @return 
+     * Description: Gets and returns the TimeSpan object for Tuesday.
+     *
+     * @return a reference to the TimeSpan object for Tuesday
      */
     public TimeSpan getTuesdayTimes() {
         return this.tuesday;
     }
-    
+
     /**
-     * 
-     * @return 
+     * Description: Gets and returns the TimeSpan object for Wednesday.
+     *
+     * @return a reference to the TimeSpan object for Wednesday
      */
     public TimeSpan getWednesdayTimes() {
         return this.wednesday;
     }
-    
+
     /**
-     * 
-     * @return 
+     * Description: Gets and returns the TimeSpan object for Thursday.
+     *
+     * @return a reference to the TimeSpan object for Thursday
      */
     public TimeSpan getThursdayTimes() {
         return this.thursday;
     }
-    
+
     /**
-     * 
-     * @return 
+     * Description: Gets and returns the TimeSpan object for Friday.
+     *
+     * @return a reference to the TimeSpan object for Friday
      */
     public TimeSpan getFridayTimes() {
         return this.friday;
     }
-    
+
     /**
-     * 
-     * @return 
+     *
+     * @return
      */
     public String fullTimeTableString() {
         return null;
     }
-    
+
     /**
-     * 
-     * @return 
+     *
+     * @return
      */
     @Override
     public String toString() {
         return null;
     }
-    
+
     /**
-     * 
-     * @param args 
+     *
+     * @param args
      */
     public static void main(String[] args) {
-        
+
     }
 
 }
