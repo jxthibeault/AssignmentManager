98a43e4c3c025993953a02d194867cb36d20aa62
Josh Thibeault <jxthibeault@gmail.com>
2018-12-06 22:50:46 -0500

    Created skeleton files for all required classes

---
+++ src/AssignmentManager/AssignmentManager.java
@@ -0,0 +1,15 @@
+package AssignmentManager;
+
+/**
+ * @author Josh Thibeault
+ */
+public class AssignmentManager {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
