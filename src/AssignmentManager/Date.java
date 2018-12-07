c931009ef56a6ae869038615a3647f8c0b26a9ec
Josh Thibeault <jxthibeault@gmail.com>
2018-12-06 22:55:28 -0500

    Wrote class skeleton and method stubs for Date

--- src/AssignmentManager/Date.java
+++ src/AssignmentManager/Date.java
@@ -1,14 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package AssignmentManager;
 
 /**
- *
+ * Description: A class to represent a specific date as a day, month, and year.
  * @author Josh Thibeault
  */
-public class Date {
+public class Date implements Comparable<Date> {
+    private int day;
+    private int month;
+    private int year;
     
+    public Date() {
+        
+    }
+    
+    public Date(int day, int month, int year) {
+        
+    }
+    
+    public int getDay() {
+        return 0;
+    }
+    
+    public int getMonth() {
+        return 0;
+    }
+    
+    public int getYear() {
+        return 0;
+    }
+    
+    @Override
+    public int compareTo(Date that) {
+        return 0;
+    }
+    
+    public String toString() {
+        return null;
+    }
+    
+    public static void main(String[] args) {
+        
+    }
 }
