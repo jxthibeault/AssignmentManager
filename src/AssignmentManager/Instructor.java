a0bf2ebde37580adc75f0c69013517df08f25bb6
Josh Thibeault <jxthibeault@gmail.com>
2018-12-15 14:42:14 -0500

    Updated Instructor in UML to reflect actual implementation. Wrote skeleton and method stubs with empty Javadoc for Instructor class.

--- src/AssignmentManager/Instructor.java
+++ src/AssignmentManager/Instructor.java
@@ -1,14 +1,102 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package AssignmentManager;
 
 /**
+ * Description: A class to represent an instructor of a course, including
+ * contact information and office hours.
  *
  * @author Josh Thibeault
  */
-public class Instructor {
+public final class Instructor implements Comparable<Instructor> {
     
+    private final String name;
+    private final String email;
+    private final String phone;
+    private final TimeTable officeHours;
+    
+    /**
+     * 
+     */
+    public Instructor() {
+        
+    }
+    
+    /**
+     * 
+     * @param name 
+     */
+    public Instructor(String name) {
+        
+    }
+    
+    /**
+     * 
+     * @param name
+     * @param email
+     * @param phone
+     * @param officeHours 
+     */
+    public Instructor(String name, String email, String phone,
+            TimeTable officeHours) {
+        
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    public String getName() {
+        return this.name;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    public String getEmail() {
+        return this.email;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    public String getPhone() {
+        return this.phone;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    public TimeTable getOfficeHours() {
+        return this.officeHours;
+    }
+    
+    /**
+     * 
+     * @param that
+     * @return 
+     */
+    @Override
+    public int compareTo(Instructor that) {
+        return 0;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    @Override
+    public String toString() {
+        return null;
+    }
+    
+    /**
+     * 
+     * @param args 
+     */
+    public static void main(String[] args) {
+        
+    }
+
 }
