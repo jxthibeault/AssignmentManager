41e9f8f98c9d13f95ca60943568221a3b6e3e0a7
Josh Thibeault <jxthibeault@gmail.com>
2018-12-15 15:14:15 -0500

    Designed and wrote compareTo method for Instructor with unit tests (pass).

--- src/AssignmentManager/Instructor.java
+++ src/AssignmentManager/Instructor.java
@@ -7,35 +7,35 @@ package AssignmentManager;
  * @author Josh Thibeault
  */
 public final class Instructor implements Comparable<Instructor> {
-    
+
     private final String name;
     private final String email;
     private final String phone;
     private final TimeTable officeHours;
-    
+
     /**
      * Description: Creates an empty Instructor object to use when allocating
-     * memory. Note that Instructor is an immutable class, and using the
-     * default constructor serves no purpose beyond memory allocation.
+     * memory. Note that Instructor is an immutable class, and using the default
+     * constructor serves no purpose beyond memory allocation.
      */
     public Instructor() {
-        this(null); 
+        this(null);
     }
-    
+
     /**
      * Description: Creates an Instructor object that has a name, but no contact
      * information or office hours.
-     * 
+     *
      * @param name name of the instructor
      */
     public Instructor(String name) {
         this(name, null, null, null);
     }
-    
+
     /**
      * Description: Creates an Instructor object with a name, email address,
      * phone number, and office hours.
-     * 
+     *
      * @param name name of the instructor
      * @param email instructor email address
      * @param phone instructor office phone number
@@ -46,66 +46,105 @@ public final class Instructor implements Comparable<Instructor> {
         this.name = name;
         this.email = email;
         this.phone = phone;
-        this.officeHours = officeHours;        
+        this.officeHours = officeHours;
     }
-    
+
     /**
-     * 
-     * @return 
+     * Description: Gets and returns the instructor's name.
+     *
+     * @return the name of the instructor
      */
     public String getName() {
         return this.name;
     }
-    
+
     /**
-     * 
-     * @return 
+     * Description: Gets and returns the instructor's email.
+     *
+     * @return email address of the instructor
      */
     public String getEmail() {
         return this.email;
     }
-    
+
     /**
-     * 
-     * @return 
+     * Description: Gets and returns the instructor's phone number.
+     *
+     * @return phone number of the instructor
      */
     public String getPhone() {
         return this.phone;
     }
-    
+
     /**
-     * 
-     * @return 
+     * Description: Gets and returns the instructor's office hours.
+     *
+     * @return time table of the instructor's office hours
      */
     public TimeTable getOfficeHours() {
         return this.officeHours;
     }
-    
+
     /**
-     * 
-     * @param that
-     * @return 
+     * Description: Compares this Instructor object with the specified
+     * Instructor object for order. Returns a negative integer, zero, or a
+     * positive integer as this Instructor's name is before, the same as, or
+     * after the specified Instructor's name, alphabetically. Casing of names is
+     * ignored when making these comparisons.
+     *
+     * @param that the Instructor to compare this Instructor to
+     * @return negative integer, zero, or positive integer
      */
     @Override
     public int compareTo(Instructor that) {
+        String thatName = that.getName().toLowerCase();
+        String thisName = this.name.toLowerCase();
+
+        int shorterNameLength;
+
+        if (thatName.length() < thisName.length()) {
+            shorterNameLength = thatName.length();
+        } else {
+            shorterNameLength = thisName.length();
+        }
+
+        for (int i = 0; i < shorterNameLength; i++) {
+            if (thisName.toCharArray()[i] < thatName.toCharArray()[i]) {
+                return -1;
+            } else if (thisName.toCharArray()[i]
+                    > thatName.toCharArray()[i]) {
+                return 1;
+            }
+        }
+
         return 0;
+
     }
-    
+
     /**
-     * 
-     * @return 
+     *
+     * @return
      */
     @Override
     public String toString() {
         return null;
     }
-    
+
     /**
-     * 
-     * @param args 
+     *
+     * @param args
      */
     public static void main(String[] args) {
         
+        Instructor testOne = new Instructor("John Smith");
+        Instructor testTwo = new Instructor("John Smath");
+        Instructor testThree = new Instructor("Jon Smith");
+        Instructor testFour = new Instructor("John Smith");
+        
+        System.out.println(testOne.compareTo(testTwo));
+        System.out.println(testOne.compareTo(testThree));
+        System.out.println(testOne.compareTo(testFour));
+
     }
 
 }
