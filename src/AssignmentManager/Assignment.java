f5e9f63c70c9e5ada7fb71fbd720379693b5eabd
Josh Thibeault <jxthibeault@gmail.com>
2018-12-15 15:51:12 -0500

    Designed and wrote constructor and completed getter javadoc for Assignment class.

--- src/AssignmentManager/Assignment.java
+++ src/AssignmentManager/Assignment.java
@@ -7,22 +7,26 @@ package AssignmentManager;
  * @author Josh Thibeault
  */
 public class Assignment implements Comparable<Assignment> {
-    
+
     private final int assignmentID;
     private final String name;
     private final String reading;
     private final Date dueDate;
     private final String gradeCategory;
     private boolean complete;
-    
+
     /**
-     * 
-     * @param assignmentID
-     * @param name
-     * @param reading
-     * @param dueDate
-     * @param gradeCategory
-     * @param complete 
+     * Description: Creates a new Assignment object with an assignment ID, name,
+     * related readings, due date, grading category, and completion status. Note
+     * that under ideal usage, assignment ID should be unique to each Assignment
+     * object.
+     *
+     * @param assignmentID unique identifier for this assignment
+     * @param name name or brief description of assignment
+     * @param reading associated or required readings for the assignment
+     * @param dueDate date by which assignment is due
+     * @param gradeCategory category on grading rubric assignment belongs to
+     * @param complete whether or not assignment is complete
      */
     public Assignment(int assignmentID, String name, String reading,
             Date dueDate, String gradeCategory, boolean complete) {
@@ -33,91 +37,99 @@ public class Assignment implements Comparable<Assignment> {
         this.gradeCategory = gradeCategory;
         this.complete = complete;
     }
-    
+
     /**
-     * 
-     * @return 
+     * Description: Gets and returns this assignment's ID.
+     *
+     * @return assignment identifier of the assignment
      */
     public int getAssignmentID() {
         return this.assignmentID;
     }
-    
+
     /**
-     * 
-     * @return 
+     * Description: Gets and returns the name or brief description of this
+     * assignment.
+     *
+     * @return name or brief description of the assignment
      */
     public String getName() {
         return this.name;
     }
-    
+
     /**
-     * 
-     * @return 
+     * Description: Gets and returns the associated or required readings for
+     * this assignment.
+     *
+     * @return associated or required readings for the assignment
      */
     public String getReading() {
         return this.reading;
     }
-    
+
     /**
-     * 
-     * @return 
+     * Description: Gets and returns the due date of this assignment.
+     *
+     * @return due date of the assignment
      */
     public Date getDueDate() {
         return this.dueDate;
     }
-    
+
     /**
-     * 
-     * @return 
+     * Description: Gets and returns the grading category to which this
+     * assignment belongs.
+     *
+     * @return grading category to which this assignment belongs
      */
     public String getGradeCategory() {
         return this.gradeCategory;
     }
-    
+
     /**
-     * 
-     * @return 
+     * Description: Gets and returns the completion status of this assignment.
+     *
+     * @return true if assignment is complete, otherwise false
      */
     public boolean isComplete() {
         return this.complete;
     }
-    
+
     /**
-     * 
+     * Description: Marks this assignment as complete, regardless of prior
+     * completion status.
      */
     public void markComplete() {
         this.complete = true;
     }
-    
+
     /**
-     * 
+     *
      * @param that
-     * @return 
+     * @return
      */
     @Override
     public int compareTo(Assignment that) {
         return 0;
     }
-    
+
     /**
-     * 
-     * @return 
+     *
+     * @return
      */
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
-        
-        
-        
+
         return builder.toString();
     }
-    
+
     /**
-     * 
-     * @param args 
+     *
+     * @param args
      */
     public static void main(String[] args) {
-        
+
     }
-    
+
 }
