8caed3f9c74c880e9c56cdfdf8885873bdf4be45
Josh Thibeault <jxthibeault@gmail.com>
2018-12-15 15:00:50 -0500

    Designed and wrote constructors with documentation for Instructor

--- src/AssignmentManager/Instructor.java
+++ src/AssignmentManager/Instructor.java
@@ -14,30 +14,39 @@ public final class Instructor implements Comparable<Instructor> {
     private final TimeTable officeHours;
     
     /**
-     * 
+     * Description: Creates an empty Instructor object to use when allocating
+     * memory. Note that Instructor is an immutable class, and using the
+     * default constructor serves no purpose beyond memory allocation.
      */
     public Instructor() {
-        
+        this(null); 
     }
     
     /**
+     * Description: Creates an Instructor object that has a name, but no contact
+     * information or office hours.
      * 
-     * @param name 
+     * @param name name of the instructor
      */
     public Instructor(String name) {
-        
+        this(name, null, null, null);
     }
     
     /**
+     * Description: Creates an Instructor object with a name, email address,
+     * phone number, and office hours.
      * 
-     * @param name
-     * @param email
-     * @param phone
-     * @param officeHours 
+     * @param name name of the instructor
+     * @param email instructor email address
+     * @param phone instructor office phone number
+     * @param officeHours time table of instructor office hours
      */
     public Instructor(String name, String email, String phone,
             TimeTable officeHours) {
-        
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.officeHours = officeHours;        
     }
     
     /**
